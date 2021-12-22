0e777d851e23db9adc05febb6bc95120b663cb75
pisenliang <pisenliang@gmail.com>
2021-12-22 11:00:50 +0800

    download 错误修复

--- src/model/download.js
+++ src/model/download.js
@@ -5,16 +5,16 @@
  * @Website: https://senliangpi.github.io/blog/#/
  * @Date: 2020-11-20 10:39:38
  * @LastEditors: Pi Patle
- * @LastEditTime: 2021-04-21 15:50:06
+ * @LastEditTime: 2021-12-22 10:57:58
  */
 //web 文件下载使用
 import { getObjectURL } from './getObjectURL.js'
-export function download(url){
+export function download(url,name){
   fetch(url).then(res => res.blob().then(blob => {
     let download = document.createElement('a'),
     url = getObjectURL(blob);
     download.href = url;
-    download.download = request.retData.fileName;
+    download.download = name;
     download.click();
   }))
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,12 @@
+/*
+ * @Author: Pi Patle
+ * @Email: pisenliang@gmail.com
+ * @Github: https://github.com/SenLiangpi
+ * @Website: https://senliangpi.github.io/blog/#/
+ * @Date: 2021-10-21 14:27:36
+ * @LastEditors: Pi Patle
+ * @LastEditTime: 2021-11-04 17:48:03
+ */
 const path = require('path');
 
 module.exports = {
@@ -8,5 +17,8 @@ module.exports = {
     publicPath: '/lib',
     libraryTarget: 'umd',
     umdNamedDefine: true
-  }
+  },
+  plugins: [
+    
+  ]
 }
